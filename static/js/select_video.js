1a34ac39df167c723655f239cd390c139d2bef8e
SZB3748 <jakengoodyear@gmail.com>
2024-02-28 15:46:52 -0500

    Added submission handling for video trim form
    The data is collected, excluding the video url/contents, but it isn't sent anywhere yet. The data will most likely be posted using `fetch`, then handled by a service worker.

--- static/js/select_video.js
+++ static/js/select_video.js
@@ -14,16 +14,64 @@ const PREVIEW_YOUTUBE = "preview-youtube";
 
 const YOUTUBE_URLS = ["www.youtube.com", "youtube.com", "youtu.be"];
 
+/**
+ * Makes the video select form visible and the video trib form not visible
+ */
 function setMenuModeSelect() {
     const videoMenu = document.querySelector(".video-menu");
     videoMenu.classList.remove(MODE_TRIM);
     videoMenu.classList.add(MODE_SELECT);
 }
 
+/**
+ * Makes the video trim form visible and the video select form not visible
+ */
 function setMenuModeTrim() {
     const videoMenu = document.querySelector(".video-menu");
     videoMenu.classList.remove(MODE_SELECT);
     videoMenu.classList.add(MODE_TRIM);
+}
+
+/**
+ * Convert a video timestamp to seconds since the start of the video
+ * @param {string} timestamp The timestamp to convert
+ * @returns {number} The number of seconds 
+ */
+function convertTimestamp(timestamp) {
+    if (!timestamp.includes(":")) //if there is no colon, then its just seconds
+        return Number(timestamp);
+
+    const parts = timestamp.split(":");
+    let total = 0;
+    for (let i = 0; i < parts.length; i++)
+        total += Number(parts[parts.length-i-1]) * Math.pow(60, i);
+
+    return total;
+}
+
+/**
+ * Check if the duration input `elm` is valid
+ * @param {HTMLInputElement} elm The element to check
+ * @returns {boolean} If the input is valid
+ */
+function checkDurationInput(elm) {
+    if (elm.disabled) {
+        elm.setCustomValidity("");
+        return true;
+    }
+    const val = Number(elm.value);
+    if (val === NaN) {
+        elm.setCustomValidity("Must be a number.");
+        return false;
+    }
+    else if (val < 0) {
+        elm.setCustomValidity("Cannot be negative.");
+        return false;
+    }
+    else {
+        elm.setCustomValidity("");
+        return true;
+    }
 }
 
 window.addEventListener("load", () => {
@@ -36,31 +84,48 @@ window.addEventListener("load", () => {
     /** @type {HTMLButtonElement} */
     const cancelTrim = document.getElementById(ID_CANCEL_TRIM);
 
-    //input event clears any invalidity status
+    //get inputs by name
 
-    videoSelectForm.querySelectorAll("input").forEach(elm => {
-        elm.addEventListener("input", () => {
-            elm.setCustomValidity("");
-        });
+    //video select form
+    /** @type {HTMLInputElement} */
+    const fileInput = videoSelectForm.querySelector("input[name=\"file\"]");
+    /** @type {HTMLInputElement} */
+    const urlInput = videoSelectForm.querySelector("input[name=\"url\"]");
+
+    //video trim form
+    /** @type {HTMLInputElement} */
+    const startInput = videoTrimForm.querySelector("input[name=\"start\"]");
+    /** @type {HTMLInputElement} */
+    const durationInput = videoTrimForm.querySelector("input[name=\"duration\"]");
+    /** @type {HTMLInputElement} */
+    const wholeVideoInput = videoTrimForm.querySelector("input[name=\"whole_video\"]");
+
+
+    //add input event listeners
+
+    fileInput.addEventListener("input", () => { fileInput.setCustomValidity(""); });
+    urlInput.addEventListener("input", () => { urlInput.setCustomValidity(""); });
+    startInput.addEventListener("input", () => { startInput.setCustomValidity(""); });
+
+    durationInput.addEventListener("input", () => {
+        checkDurationInput(durationInput);
     });
-    
-    videoTrimForm.querySelectorAll("input").forEach(elm => {
-        elm.addEventListener("input", () => {
-            elm.setCustomValidity("");
-        });
+
+    //enable and disable duration input when toggled
+    wholeVideoInput.addEventListener("input", () => {
+        if (wholeVideoInput.checked)
+            durationInput.disabled = true;
+        else {
+            durationInput.disabled = false;
+            checkDurationInput(durationInput);
+        }
     });
+
 
     //set submission hander for select form (select -> trim)
 
     videoSelectForm.addEventListener("submit", (ev) => {
         ev.preventDefault();
-
-        //get inputs by name
-
-        /** @type {HTMLInputElement} */
-        const fileInput = videoSelectForm.querySelector("input[name=\"file\"]");
-        /** @type {HTMLInputElement} */
-        const urlInput = videoSelectForm.querySelector("input[name=\"url\"]");
 
         if (fileInput.reportValidity() && fileInput.files.length) {
 
@@ -142,6 +207,27 @@ window.addEventListener("load", () => {
     videoTrimForm.addEventListener("submit", (ev) => {
         ev.preventDefault();
 
+        const startSeconds = convertTimestamp(startInput.value);
+        if (startSeconds === NaN) {
+            startInput.setCustomValidity("Not a valid number or timestamp.");
+            return;
+        }
+        else if (startSeconds < 0) {
+            startInput.setCustomValidity("Seconds or timestamp cannot be negative.");
+            return;
+        }
+
+        if (!checkDurationInput(durationInput) || !durationInput.reportValidity())
+            return;
+
+        //TODO get video url from preview (check classes to retrieve properly)
+
+        startInput.value = startSeconds;
+
+        const data = new FormData(videoTrimForm);
+        //TODO data.append("video", url.href); or something like that
+
+        //TODO construct url to post to, handled by service worker
     });
 
     //cancel button for trim form (select <- trim)
