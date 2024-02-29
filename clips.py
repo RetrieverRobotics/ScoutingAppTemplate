aa61e750efe34c69304ac7e27771ec95454df22d
SZB3748 <jakengoodyear@gmail.com>
2024-02-28 21:53:10 -0500

    Add clip request handlers

--- clips.py
+++ clips.py
@@ -1,4 +1,6 @@
 from datetime import datetime, timezone
+from flask import Blueprint, send_file
+import json
 import os
 
 CLIPS_DIR = "clips"
@@ -6,6 +8,8 @@ CLIPS_DIR = "clips"
 GROUP_NAME_SEP = "."
 GROUP_NAME_DATE_FORMAT = "%Y-%m-%d"
 CLIP_NAME_PREFIX = "match_"
+
+bp = Blueprint("clips", __name__, url_prefix="/clips")
 
 def parse_group_name(name:str)->tuple[str, datetime]:
     """Parses a group name into a competition name and the utc datetime for when the competition happened."""
@@ -53,3 +57,15 @@ def construct_path(comp_name:str, dt:datetime, match:int, file_format:str|None=N
     group_name = format_group_name(comp_name, dt)
     clip_name = format_clip_name(match, file_format)
     return os.path.join(group_name, clip_name)
+
+#request handlers
+
+@bp.get("/tree")
+def get_clips_tree():
+    tree = read_clips_tree(as_paths=False)
+    return json.dumps(tree), 200, {"Content-Type":"application/json; charset=utf-8"}
+
+@bp.get("/load/<group_name>/<clip_name>")
+def load_clip(group_name:str, clip_name:str):
+    path = os.path.join(CLIPS_DIR, group_name, clip_name)
+    return send_file(path, download_name=clip_name)
