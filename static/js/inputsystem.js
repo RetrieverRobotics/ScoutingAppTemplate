1742fa4154e1ce26231700c733c6517ac557501c
SZB3748 <jakengoodyear@gmail.com>
2024-03-13 22:23:05 -0400

    Add submission to the input system

--- static/js/inputsystem.js
+++ static/js/inputsystem.js
@@ -5,8 +5,60 @@ const CLASS_INPUT_SYSTEM_NAVIGATE = "input-system-navigate";
 const CLASS_INPUT_SYSTEM_SUBMIT = "input-system-submit";
 
 const EVENT_NAVIGATE = "InputSystem::navigate";
+const EVENT_SUBMIT = "InputSystem::submit";
 
 const ATTR_PAGE = "page";
+
+/**
+ * @typedef {object} InputSystemNavigateEvent 
+ * @extends Event
+ * @property {string} page 
+ */
+
+/**
+ * @typedef {Event} InputSystemSubmissionEvent Event for submitting data collected by the InputSystem
+ */
+
+/**
+ * A custom InputSystem event
+ */
+class InputSystemEvent extends Event {
+    /**
+     * @param {string} type Event name
+     * @param {EventInit|undefined} init Event initialization options; includes `inputSystem` property
+     */
+    constructor(type, init) {
+        const sys = init?.inputSystem || null;
+        delete init.inputSystem;
+        super(type, init);
+        /** @type {InputSystem} InputSystem that this event is relevant to */
+        this.inputSystem = sys;
+    }
+}
+
+/**
+ * Input System event dispatched to Input System inputs of the current page
+ * before navigating to another Input System page
+ */
+class InputSystemNavigateEvent extends InputSystemEvent {
+    /**
+     * @param {string} type Event name
+     * @param {EventInit|undefined} init Event initialization options; includes `page` property
+     */
+    constructor(type, init) {
+        const page = init?.page || null;
+        delete init.page;
+        super(type, init);
+        /** @type {string} Name of the page being navigated to */
+        this.page = page;
+    }
+}
+
+/**
+ * Input System event dispatched to all Input System inputs before submitting
+ * the collected data
+ */
+class InputSystemSubmissionEvent extends InputSystemEvent { }
 
 class InputSystem {
     /**
@@ -18,7 +70,7 @@ class InputSystem {
         this.root = root || document.documentElement;
         /** @type {Map<string, any>} */
         this.data = new Map();
-        
+
         this.bound = {}
         this.bound.clearValidity = this.clearValidity.bind(this);
         this.bound.saveInput = this.saveInput.bind(this);
@@ -46,17 +98,16 @@ class InputSystem {
     }
 
     /**
-     * Select each navigation/submission element and apply the appropriate event listeners
+     * Select each navigation element and apply the appropriate event listeners
      * to it. Requires that the navigation element has a `page` attribute set to the desired
      * name of the page to navigate to.
      */
     applyNavigation() {
         this.root.querySelectorAll(`.${CLASS_INPUT_SYSTEM_PAGE}`).forEach(page => {
             page.querySelectorAll(`.${CLASS_INPUT_SYSTEM_NAVIGATE}`).forEach(elm => {
-                elm.addEventListener("click", (ev) => {
+                elm.addEventListener("click", () => {
                     const pagename = elm.getAttribute(ATTR_PAGE);
-                    const navigateEvent = new Event(EVENT_NAVIGATE, {bubbles:false, cancelable:true});
-                    navigateEvent.page = pagename; //TODO JSDoc so that suggestions work for InputSystemNavigateEvent.page
+                    const navigateEvent = new InputSystemNavigateEvent(EVENT_NAVIGATE, {bubbles:false, cancelable:true, page:pagename});
 
                     let doNav = true;
                     const inputs = page.querySelectorAll(`.${CLASS_INPUT_SYSTEM_INPUT}`);
@@ -67,6 +118,33 @@ class InputSystem {
 
                     this.setCurrentPage(pagename);
                 });
+            });
+        });
+    }
+    
+    /**
+     * Select each submission element and apply the appropriate event listeners to it.
+     * The submission event listener will dispatch an event to each element in the input system's root marked with
+     * the `input-system-input` class.
+     * @param {function|undefined} success An optional callback with no parameters-- called when submission was not canceled and there were no invalid inputs
+     * @param {function|undefined} cancel An optional callback with no parameters-- called when submission was interrupted
+     */
+    applySubmission(success, cancel) {
+        this.root.querySelectorAll(`.${CLASS_INPUT_SYSTEM_SUBMIT}`).forEach(submittionElm => {
+            submittionElm.addEventListener("click", () => {
+                const inputs = this.root.querySelectorAll(`.${CLASS_INPUT_SYSTEM_INPUT}`);
+                const submitEvent = new InputSystemSubmissionEvent(EVENT_SUBMIT, {bubbles:false, cancelable:true})
+
+                let doSubmit = true;
+                for (let i = 0; i < inputs.length; i++)
+                    doSubmit = inputs[i].dispatchEvent(submitEvent) && (!"reportValidity" in inputs[i] || inputs[i].reportValidity()) && doSubmit;
+
+                if (!doSubmit) return; //cancelled / doesnt get submitted anywhere
+
+                if (doSubmit && success)
+                    success();
+                else if (!doSubmit && cancel)
+                    cancel(); //NOTE: to get invalid inputs, use `inputSystem.root.querySelectAll(".input-system-input:invalid")`
             });
         });
     }
